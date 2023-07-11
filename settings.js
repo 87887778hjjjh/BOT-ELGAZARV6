1fb55f72c54c3d085b4efa804e6f0a7a6c49732c
Bnhaym <138430389+87887778hjjjh@users.noreply.github.com>
2023-07-11 07:18:37 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -15,23 +15,23 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.owner = ['201098906252'] //ur owner number
-global.ownername = "ᗴᒪᘜᗩᘔᗩᖇ ᗩᒪᗯᗩᘔᗴᖇ❤️" //ur owner name
-global.ytname = "YT: ELGAZAR" //ur yt chanel name
-global.socialm = "GitHub: ELGAZAR" //ur github or insta name
-global.location = "Africa, Egypt, Cairo" //ur location
+global.owner = ['967780411705'] //ur owner number
+global.ownername = "BN.HAIM" //ur owner name
+global.ytname = "YT: BN.HAIM" //ur yt chanel name
+global.socialm = "GitHub: BN.HAIM " //ur github or insta name
+global.location = "Africa, يمن, عدن" //ur location
 
 //bot bomdy 
-global.ownernomer = "201098906252" //ur number
-global.premium = ['201098906252'] //ur premium number
-global.botname = '❤️ᗴᒪᘜᗩᘔᗩᖇ ᗷOT❤️' //ur bot name
-global.linkz = "https://youtube.com/@ABDALLAH_MOHAMED" //your theme url which will be displayed on whatsapp
-global.websitex = "https://telegra.ph/file/ec763cc3e7c88d0e67682.jpg" //ur website to be displayed
+global.ownernomer = "967780411705" //ur number
+global.premium = ['967780411705'] //ur premium number
+global.botname = 'BN.HAIM ᗷOT' //ur bot name
+global.linkz = "" //your theme url which will be displayed on whatsapp
+global.websitex = "https://chat.whatsapp.com/BBqkLV10ubA7hxmWdx1zVy" //ur website to be displayed
 global.botscript = 'https://youtube.com/@ABDALLAH_MOHAMED' //script link
-global.themeemoji = "♥" //ur theme emoji
-global.packname = "⍣الجزار الوزير يروحي⍣" //ur sticker watermark packname
-global.author = "⍣01098906252⍣" //ur sticker watermark author
-global.wm = "❤️ᗴᒪᘜᗩᘔᗩᖇ ᗷOT❤️." //ur watermark
+global.themeemoji = "" //ur theme emoji
+global.packname = "⍣  الكبسه حلوه ⍣" //ur sticker watermark packname
+global.author = "⍣967780411705⍣" //ur sticker watermark author
+global.wm = "BN.HAIM ᗷOT." //ur watermark
 
 // Other
 global.sessionName = 'session'
